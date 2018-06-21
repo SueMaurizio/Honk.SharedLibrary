a5f30a204aed379e43c4923c1cca426c82c54657
Sue <sue.maurizio@gmail.com>
2018-06-21 22:45:50 +0200

    First commit

---
+++ app/src/main/java/org/honk/sharedlibrary/UIHelper.java
@@ -0,0 +1,17 @@
+package org.honk.sharedlibrary;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class UIHelper {
+    public static void showAlert(String message, Context context) {
+        AlertDialog dialog = new AlertDialog.Builder(context)
+                .setMessage(message)
+                .setTitle(R.string.alertTitle)
+                .setPositiveButton(R.string.close, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {}
+                })
+                .show();
+    }
+}
